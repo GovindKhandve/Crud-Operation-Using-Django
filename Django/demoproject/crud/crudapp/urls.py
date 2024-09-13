8e11a2637c54ed9726529db954124cb3aa043c62
GovindKhandve <145098459+GovindKhandve@users.noreply.github.com>
2024-09-13 13:22:07 +0530

    Add files via upload

---
+++ Django/demoproject/crud/crudapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path,include
+from . import views
+urlpatterns = [
+    path('',views.index),
+    path('add/',views.add,name='add'),
+    path('edit',views.edit,name='edit'),
+    path('update/<int:id>',views.update,name='update'),
+    path('delete/<str:id>',views.delete,name='delete'),
+]
